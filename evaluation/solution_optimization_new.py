b95bd0f5cec1892028519ff94857913ae362d73d
Leon-Leyang <leyang_hu@brown.edu>
2024-09-26 21:31:59 -0400

    Refactor optimization to be a method

--- evaluation/solution_optimization_new.py
+++ evaluation/solution_optimization_new.py
@@ -66,183 +66,95 @@ def extract_answer(paragraph):
     matches = re.findall(pattern, paragraph, re.DOTALL)
     return matches
 
+GRADIENT_TEMPLATE = """
+Here is answer1: {answer1}
+Here is evaluation1: {evaluation1}
+Here is answer2: {answer2}
+Here is evaluation2: {evaluation2}
+"""
+
+OPTIMIZATION_TEMPLATE = """
+Here is answer1: {answer1}
+Here is evaluation1: {evaluation1}
+Here is answer2: {answer2}
+Here is evaluation2: {evaluation2}
+Here is the analysis: {gradient}
+"""
+
+GRADIENT_COMPUTOR_SYSTEM_PROMPT = """
+You are part of an optimization system that improves a given answer (i.e. answer2). You are the gradient (feedback) engine.
+Your only responsibility is to give intelligent and creative feedback and constructive criticism to variables.
+You will be given two answers to a same question (answer1 and answer2), and the evaluation for each answer (evaluation1 and evaluation2).
+Analyze how the difference in the two evaluations(evaluation2 - evaluation1) are caused by the difference in the answers (answer2 - answer1).
+"""
+
+OPTIMIZER_SYSTEM_PROMPT = """
+You are part of an optimization system that improves text (i.e., answer2).
+You will receive two answers to a question (answer1 and answer2), two evaluations for each answer (evaluation1 and evaluation2) and an analysis of how the evaluation over answer2 being different from answer1 (evaluation2 - evaluation1) is caused by the difference in the answers (answer2 - answer1).
+Based on the analysis, you should provide a new version of answer2 that improves the worse aspects of answer2 and keep the better aspects of answer2 comparing to answer1.
+You can do this step by step, but make sure finally you provide the new version of answer2 between <answer> and </answer> tags.
+"""
+
+
+def optimize(var1, var2, gradient_computor, optimizer, test_time_objective):
+    evaluation_1 = test_time_objective(var1)
+    evaluation_2 = test_time_objective(var2)
+
+    gradient = gradient_computor(tg.Variable(
+        GRADIENT_TEMPLATE.format(answer1=var1.value, evaluation1=evaluation_1.value,
+                                 answer2=var2.value, evaluation2=evaluation_2.value),
+        role_description="gradient computation"))
+
+    optimizer_response = optimizer(tg.Variable(
+        OPTIMIZATION_TEMPLATE.format(answer1=var1.value, evaluation1=evaluation_1.value,
+                                     answer2=var2.value, evaluation2=evaluation_2.value,
+                                     gradient=gradient.value), role_description="optimizer response"))
+
+    resp = extract_answer(optimizer_response.value)
+
+    if len(resp) == 0:
+        new_answer = optimizer_response.value
+    else:
+        new_answer = resp[0]
+
+    var1 = tg.Variable(var2.value, requires_grad=False, role_description=var2.role_description)
+    var2 = tg.Variable(new_answer, requires_grad=False, role_description=var2.role_description)
+
+    return var1, var2
+
+
 def run_test_time_training(sample):
-    GRADIENT_TEMPLATE = """
-    Here is answer1: {answer1}
-    Here is evaluation1: {evaluation1}
-    Here is answer2: {answer2}
-    Here is evaluation2: {evaluation2}
-    """
-
-    OPTIMIZATION_TEMPLATE = """
-    Here is answer1: {answer1}
-    Here is evaluation1: {evaluation1}
-    Here is answer2: {answer2}
-    Here is evaluation2: {evaluation2}
-    Here is the analysis: {gradient}
-    """
-
-    GRADIENT_COMPUTOR_SYSTEM_PROMPT = """
-    You are part of an optimization system that improves a given answer (i.e. answer2). You are the gradient (feedback) engine.
-    Your only responsibility is to give intelligent and creative feedback and constructive criticism to variables.
-    You will be given two answers to a same question (answer1 and answer2), and the evaluation for each answer (evaluation1 and evaluation2).
-    Analyze how the difference in the two evaluations(evaluation2 - evaluation1) are caused by the difference in the answers (answer2 - answer1).
-    """
     gradient_computor = tg.BlackboxLLM(llm_engine, GRADIENT_COMPUTOR_SYSTEM_PROMPT)
-
-    OPTIMIZER_SYSTEM_PROMPT = """
-    You are part of an optimization system that improves text (i.e., answer2).
-    You will receive two answers to a question (answer1 and answer2), two evaluations for each answer (evaluation1 and evaluation2) and an analysis of how the evaluation over answer2 being different from answer1 (evaluation2 - evaluation1) is caused by the difference in the answers (answer2 - answer1).
-    Based on the analysis, you should provide a new version of answer2 that improves the worse aspects of answer2 and keep the better aspects of answer2 comparing to answer1.
-    You can do this step by step, but make sure finally you provide the new version of answer2 between <answer> and </answer> tags.
-    """
     optimizer = tg.BlackboxLLM(llm_engine, OPTIMIZER_SYSTEM_PROMPT)
 
     performance_history = []
+    predictions = []
+
     question, answer, test_time_objective, instance_eval_fn = sample
+
     zero_shot_response1 = get_zeroshot_answer(question)
-    
+    zero_shot_response2 = get_zeroshot_answer2(question)
     instance_var1 = tg.Variable(zero_shot_response1.value,
                                requires_grad=False,
                                role_description="creative and precise solution and the prediction for the multiple choice question")
-    
-    # Evaluate the zero-shot response
-    evaluation_1 = test_time_objective(instance_var1)
-
-
-    zero_shot_response2 = get_zeroshot_answer2(question)
     instance_var2 = tg.Variable(zero_shot_response2.value,
                                 requires_grad=False,
                                 role_description="creative and precise solution and the prediction for the multiple choice question")
+
     performance_history.append(int(instance_eval_fn(instance_var2)))
-    predictions = []
     predictions.append(tg.Variable(
         instance_var2.value,
         role_description=instance_var2.role_description
         ))
 
-    evaluation_2 = test_time_objective(instance_var2)
+    for _ in range(args.max_iterations):
+        instance_var1, instance_var2 = optimize(instance_var1, instance_var2, gradient_computor, optimizer, test_time_objective)
 
-    gradient = gradient_computor(tg.Variable(GRADIENT_TEMPLATE.format(answer1=instance_var1.value, evaluation1=evaluation_1.value, answer2=instance_var2.value, evaluation2=evaluation_2.value), role_description="gradient computation"))
-
-    print('*' * 50)
-    print('Optimization round 1')
-    print(f'Gradient: {gradient.value}')
-
-    optimizer_response = optimizer(tg.Variable(OPTIMIZATION_TEMPLATE.format(answer1=instance_var1.value, evaluation1=evaluation_1.value, answer2=instance_var2.value, evaluation2=evaluation_2.value, gradient=gradient.value), role_description="optimizer response"))
-
-    print(f'Optimizer response: {optimizer_response.value}')
-    print('*' * 50)
-
-
-    new_answer = extract_answer(optimizer_response.value)
-
-    if len(new_answer) == 0:
-        new_answer = optimizer_response.value
-        # print(f'No new answer: {new_answer}')
-    else:
-        new_answer = new_answer[0]
-
-    # iteration step
-    instance_var1 = tg.Variable(instance_var2.value,
-                                requires_grad=False,
-                                role_description="creative and precise solution and the prediction for the multiple choice question")
-    instance_var2 = tg.Variable(new_answer,
-                                requires_grad=False,
-                                role_description="creative and precise solution and the prediction for the multiple choice question")
-    performance_history.append(int(instance_eval_fn(instance_var2)))
-
-    evaluation_1 = test_time_objective(instance_var1)
-
-    predictions = []
-    predictions.append(tg.Variable(
-        instance_var2.value,
-        role_description=instance_var2.role_description
-    ))
-
-    evaluation_2 = test_time_objective(instance_var2)
-
-    gradient = gradient_computor(tg.Variable(
-        GRADIENT_TEMPLATE.format(answer1=instance_var1.value, evaluation1=evaluation_1.value,
-                                 answer2=instance_var2.value, evaluation2=evaluation_2.value),
-        role_description="gradient computation"))
-
-    print('*' * 50)
-    print('Optimization round 2')
-    print(f'Gradient: {gradient.value}')
-
-    optimizer_response = optimizer(tg.Variable(
-        OPTIMIZATION_TEMPLATE.format(answer1=instance_var1.value, evaluation1=evaluation_1.value,
-                                     answer2=instance_var2.value, evaluation2=evaluation_2.value,
-                                     gradient=gradient.value), role_description="optimizer response"))
-
-
-    print(f'Optimizer response: {optimizer_response.value}')
-    print('*' * 50)
-
-    new_answer = extract_answer(optimizer_response.value)
-
-    if len(new_answer) == 0:
-        new_answer = optimizer_response.value
-        # print(f'No new answer: {new_answer}')
-    else:
-        new_answer = new_answer[0]
-
-
-    # iteration step
-    instance_var1 = tg.Variable(instance_var2.value,
-                                requires_grad=False,
-                                role_description="creative and precise solution and the prediction for the multiple choice question")
-    instance_var2 = tg.Variable(new_answer,
-                                requires_grad=False,
-                                role_description="creative and precise solution and the prediction for the multiple choice question")
-    performance_history.append(int(instance_eval_fn(instance_var2)))
-
-    evaluation_1 = test_time_objective(instance_var1)
-
-    predictions = []
-    predictions.append(tg.Variable(
-        instance_var2.value,
-        role_description=instance_var2.role_description
-    ))
-
-    evaluation_2 = test_time_objective(instance_var2)
-
-    gradient = gradient_computor(tg.Variable(
-        GRADIENT_TEMPLATE.format(answer1=instance_var1.value, evaluation1=evaluation_1.value,
-                                 answer2=instance_var2.value, evaluation2=evaluation_2.value),
-        role_description="gradient computation"))
-
-    print('*' * 50)
-    print('Optimization round 3')
-    print(f'Gradient: {gradient.value}')
-
-    optimizer_response = optimizer(tg.Variable(
-        OPTIMIZATION_TEMPLATE.format(answer1=instance_var1.value, evaluation1=evaluation_1.value,
-                                     answer2=instance_var2.value, evaluation2=evaluation_2.value,
-                                     gradient=gradient.value), role_description="optimizer response"))
-
-
-    print(f'Optimizer response: {optimizer_response.value}')
-    print('*' * 50)
-
-    new_answer = extract_answer(optimizer_response.value)
-
-    if len(new_answer) == 0:
-        new_answer = optimizer_response.value
-        # print(f'No new answer: {new_answer}')
-    else:
-        new_answer = new_answer[0]
-
-    instance_var2 = tg.Variable(new_answer,
-                                requires_grad=False,
-                                role_description="creative and precise solution and the prediction for the multiple choice question")
-
-    performance_history.append(int(instance_eval_fn(instance_var2)))
-    predictions.append(tg.Variable(
-        instance_var2.value,
-        role_description=instance_var2.role_description
-    ))
+        performance_history.append(int(instance_eval_fn(instance_var2)))
+        predictions.append(tg.Variable(
+            instance_var2.value,
+            role_description=instance_var2.role_description
+        ))
 
     ensembled_prediction = ensembler(predictions)
     performance_history.append(instance_eval_fn(ensembled_prediction))
@@ -255,7 +167,7 @@ def run_test_time_training(sample):
 args = config()
 args.task = 'GPQA_main'
 args.engine = 'azure-gpt4o'
-args.num_threads = 1
+args.num_threads = 10
 llm_engine = tg.get_engine(engine_name=args.engine)
 tg.set_backward_engine(llm_engine, override=True)
 test_set = load_instance_task(args.task, evaluation_api=llm_engine, max_samples=10)
@@ -271,8 +183,8 @@ with concurrent.futures.ThreadPoolExecutor(max_workers=args.num_threads) as exec
     all_history = []
     for future in tqdm(concurrent.futures.as_completed(futures), total=len(futures), position=0):
         performance_history, predictions, question, answer = future.result()
-        # assert len(performance_history) == args.max_iterations + 2
-        # assert len(predictions) == args.max_iterations + 2
+        assert len(performance_history) == args.max_iterations + 2
+        assert len(predictions) == args.max_iterations + 2
         all_solutions[question] = {"predictions": [p.value for p in predictions], "answer": answer}
         all_history.append(performance_history)
 
